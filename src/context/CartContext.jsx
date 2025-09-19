9b228f667d46c7c3157a7193f1263481093b3b8a
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-19 14:08:33 +0300

    add to cart functionality

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,8 +5,21 @@ const CartContext = createContext();
 export function CartProvider({ children }) {
     const [cart, setCart] = useState([]);
 
+    const addToCart = (product) => {
+        setCart(prevState => {
+            const existing = prevState.find(item => item.id === product.id);
+
+            if (existing) {
+                return prevState.map(item => item.id === product.id ?
+                    { ...item, qty: item.qty + 1 } : item);
+            }
+
+            return [...prevState, { ...product, qty: 1 }]
+        })
+    };
+
     return (
-        <CartContext.Provider value={{ cart }}>
+        <CartContext.Provider value={{ cart, addToCart }}>
             {children}
         </CartContext.Provider>
     );
