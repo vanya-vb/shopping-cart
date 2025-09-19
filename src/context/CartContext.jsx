dffa20beba06aab95204a39dccd7577c13a31ce0
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-19 14:43:46 +0300

    store cart in local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const storedItems = localStorage.getItem('cart');
+
+        return storedItems ? JSON.parse(storedItems) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
 
     const addToCart = (product) => {
         setCart(prevState => {
